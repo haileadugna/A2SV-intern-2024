b73f9678ea4203bef49c0cd4c30fa67c0875e5d6
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2024-03-19 11:58:09 +0300

    duplicate the cards

--- task2/components/card.tsx
+++ task2/components/card.tsx
@@ -3,19 +3,18 @@ import React from "react";
 
 interface CardProps {
   title: string;
-  organization: string;
-  location: string;
+  subTitle: string;
   description: string;
   tags: string[];
-  image: StaticImageData;
+  image: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, organization, location, description, tags, image }) => {
+const Card: React.FC<CardProps> = ({ title, subTitle, description, tags, image }) => {
     return (
-        <div className="h-[266px] w-[919px] rounded-[30px] border-[1px] bg-white p-[24px] overflow-auto">
+        <div className="h-[300px] w-[919px] rounded-[30px] border-[1px] bg-white p-[24px] overflow-auto">
             <div className="flex h-[100%] w-[100%] p-[24px]">
             <div className="h-[59px] w-[66px] mr-[24px]">
-                    <Image src={image} alt="logo" />
+                    <Image src={image} alt="logo" width={200} height={200}/>
                 </div>
                 <div className="h-[100%] w-[755px] flex flex-col gap-[8px] ">
                     <div className="flex items-start gap-[8px]">
@@ -25,11 +24,11 @@ const Card: React.FC<CardProps> = ({ title, organization, location, description,
                             </div>
                             <div className="flex w-[454px] h-[27px] gap-[8px] items-center">
                                 <div className="font-epilogue font-normal text-[16px] text-[#7C8493]">
-                                    {organization}
+                                    {subTitle}
                                 </div>
                                 <div className="w-[4px] h-[4px] flex items-center justify-center bg-[#7C8493] rounded-full"></div>
                                 <div className="font-epilogue font-normal text-[16px] text-[#7C8493]">
-                                    {location}
+                                    Addis Ababa, Ethiopia
                                 </div>
                             </div>
                         </div>
@@ -37,9 +36,9 @@ const Card: React.FC<CardProps> = ({ title, organization, location, description,
                     <p className="w-[744px] h-[112px] font-epilogue text-[16px] leading-[25.6px] text-[#25324B] font-[400] text-start ">
                         {description}
                     </p>
-                    <div className="flex w-[242px] h-[31px] gap-[8px]">
+                    <div className="flex w-auto h-[31px] gap-[8px] mt-5">
                         {tags.map((tag, index) => (
-                            <div key={index} className="w-[76px] h-[31px] rounded-[80px] p-[6px] gap-[8px] flex justify-center items-center border border-[#4640DE]">
+                            <div key={index} className=" h-auto rounded-[80px] p-[6px] gap-[8px] flex justify-center items-center border border-[#4640DE]">
                                 <div className="font-epilogue font-semibold text-[12px] text-[#4640DE]">
                                     {tag}
                                 </div>
@@ -49,6 +48,7 @@ const Card: React.FC<CardProps> = ({ title, organization, location, description,
                 </div>
             </div>
         </div>
+
     );
 };
 
